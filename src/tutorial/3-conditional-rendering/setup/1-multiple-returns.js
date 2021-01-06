e2c22091a3b6c9559654b0d1bde1e330f98719b6
Tamim Jabr <tj222kg@student.lnu.se>
2021-01-06 19:30:38 +0100

    complete shortCircuit

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,48 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isloading, setIsLoading]= useState(true)
+  const [isError, setIsError]= useState(false)
+  const [user, setUser]=useState([])
+
+  const getUser= async ()=>{
+    try{
+      const response = await fetch(url)
+
+      const user=await response.json()
+      // check if we get data using the response.status
+      if(response.status>=200 && response.status < 300){
+        setUser(user)
+        setIsLoading(false)
+      }else{
+        setIsLoading(false)
+        setIsError(true)
+      }
+    }catch{
+      console.log('error while fetching')
+      // remove loading on error
+      setIsLoading(false)
+      setIsError(true)
+    }
+
+  }
+
+  useEffect(()=>{
+    getUser()
+    // call only on the initial render
+  },[])
+
+  if (isloading){
+    return <h1>Loading...</h1>
+  }
+  if(isError){
+    return(
+      <div>
+        <h1>Error...</h1>
+      </div>
+    )
+  }
+  return <h2>{user.login}</h2>;
 };
 
 export default MultipleReturns;
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -2,11 +2,32 @@ import React, { useState } from 'react';
 // short-circuit evaluation
 // ternary operator
 
-const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+// how   const secondValue = text && 'hello world'; works
+// if the first is true will return the second value
+// if the first is false will return the first value
+
+const ShortCircuit = () => {
+  const [text, setText]= useState('Tamim')
+  const [isError, setIsError]=useState(false)
+  const firstValue = text || 'hello world';
+  const secondValue = text && 'hello world';
+
+  // if doesn't work in jsx
+  return(
+    <>
+    <h1>short circuit</h1>
+    <h2>First Value : {firstValue}</h2>
+    <h2>Second Value : {secondValue}</h2>
+    <h3>{text || "It's a default value if there is no text"}</h3>
+    {text && <p>The text is true so return this p element</p>}
+    {!text && <p>the first argument is false so  this will not show up</p>}
+
+    <button className='btn' onClick={()=>setIsError(!isError)}>toggle error</button>
+    {isError && <h1>Error...</h1>}
+    {isError ? <h3>Error is true</h3> : <h3>Error is false</h3>}
+    </>
+  ) 
 };
 
 export default ShortCircuit;
