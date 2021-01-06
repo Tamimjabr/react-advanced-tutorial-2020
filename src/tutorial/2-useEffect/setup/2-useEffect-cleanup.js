450ffc384b012afacfff4100ce29a07435d9e2e5
Tamim Jabr <tj222kg@student.lnu.se>
2021-01-06 10:28:40 +0100

    complete the useEffect-cleanup

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,35 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize]=useState(window.innerWidth)
+  console.log(size)
+
+  const handleResize=()=>{
+    setSize(window.innerWidth)
+  }
+
+  useEffect(()=>{
+    window.addEventListener('resize', handleResize)
+
+    // clean up when removing the component and before the rerender
+    return ()=>{
+      window.removeEventListener('resize', handleResize)
+    }
+  })
+
+  // to add an eventlistenar only on the initial render
+  // useEffect(()=>{
+  //   window.addEventListener('resize', handleResize)
+  // },[])
+
+
+
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size} PX</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
