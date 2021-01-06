d6c8a53ff3d3fe027dac7503b91aa0e3b1fbf765
Tamim Jabr <tj222kg@student.lnu.se>
2021-01-06 10:20:09 +0100

    complete useEffect basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,21 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue]=useState(0)
+  // you can't useEffect inside an if sats, but you can if inside useEffect
+  useEffect(()=>{
+    if(value>1){
+      document.title=`React ${value}`
+    }
+
+    console.log('using effect')
+  },[value])
+
+
+  return<>
+  <h1>{value}</h1>
+  <button className="btn" onClick={()=>setValue(value+1)}>increase</button>
+  </>
 };
 
 export default UseEffectBasics;
