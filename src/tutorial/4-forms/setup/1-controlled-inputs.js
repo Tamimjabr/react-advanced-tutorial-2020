86d77336a3a437282c0382ca2b17e98d939e5008
Tamim Jabr <tj222kg@student.lnu.se>
2021-01-07 01:33:56 +0100

    complete controlled inputs

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,60 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName,setFirstName]=useState('')
+  const [email,setEmail]=useState('')
+  const [people,setPeople]=useState([])
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    // check if there is no empty string
+    if(firstName && email){
+      const person={id: new Date().getTime().toString(),
+      firstName, email}
+      setPeople((oldPeople)=>{
+        return [...oldPeople, person]
+      })
+      // empty the inputs
+      setFirstName('')
+      setEmail('')
+    }else{
+      console.log('empty values');
+    }
+  }
+  return(
+    <>
+      <form action="" className="form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="firstName">Name</label>
+          <input
+           type="text"
+            id="firstName"
+             name="firstName"
+             value={firstName}
+             onChange={(e)=>setFirstName(e.target.value)}
+            />
+          <label htmlFor="email">Email</label>
+          <input
+           type="text"
+            id="email"
+             name="email"
+             value={email}
+             onChange={(e)=>setEmail(e.target.value)}
+            />
+            </div>
+            <button type='submit'>submit</button>
+      </form>
+      {people.map((person)=>{
+        const {id, firstName, email}=person
+        return(
+          <div key={id} className='item'>
+            <h4>{firstName}</h4>
+            <p>{email}</p>
+          </div>
+        )
+      })}
+    </>
+  )
 };
 
 export default ControlledInputs;
