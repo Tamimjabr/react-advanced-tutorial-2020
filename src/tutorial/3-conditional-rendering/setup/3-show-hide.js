72edf2b6f767c4bed87b0ecaaf93acf28c7bdbec
Tamim Jabr <tj222kg@student.lnu.se>
2021-01-07 01:15:49 +0100

    complete toggle show/hide

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,37 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false)
+  return (
+    <>
+    <button className='btn' onClick={()=>setShow(!show)}> hide/show</button>
+    {show && <Item></Item>}
+    </>
+  )
 };
 
+
+const Item=()=>{
+  const [size, setSize]=useState(window.innerWidth)
+ 
+  const handleResize=()=>{
+    setSize(window.innerWidth)
+  }
+  useEffect(()=>{
+    window.addEventListener('resize', handleResize)
+    //todo when hiding and showing a component we have to have a clean up function to renove eventlistener
+    return ()=>{
+      window.removeEventListener('resize',handleResize)
+    }
+  },[])
+
+  return(
+    <>
+        <div style={{ marginTop: '2rem' }}>
+      <h1>Window</h1>
+      <h2>size : {size}</h2>
+    </div>
+    </>
+  )
+}
 export default ShowHide;
