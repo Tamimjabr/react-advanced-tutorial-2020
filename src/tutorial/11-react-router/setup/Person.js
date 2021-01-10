316d2f7ce88fb939ed3ae2cf41f267ab79ab6b98
Tamim Jabr <tj222kg@student.lnu.se>
2021-01-10 12:20:06 +0100

    complete react router

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -1,10 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { data } from '../../../data';
 import { Link, useParams } from 'react-router-dom';
+
 const Person = () => {
+  console.log(useParams())
+  const {id} = useParams()
+  console.log(id)
+  const [name, setName]=useState('default name')
+
+  useEffect(()=>{
+    const newPerson= data.find(person => person.id=== parseInt(id))
+    console.log(newPerson)
+    setName(newPerson.name)
+
+  },[])
+
   return (
     <div>
-      <h2>person</h2>
+      <h2>{name}</h2>
+      <Link to='/people' className='btn'> Back to people</Link>
     </div>
   );
 };
--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -10,7 +10,27 @@ import Person from './Person';
 // navbar
 import Navbar from './Navbar';
 const ReactRouterSetup = () => {
-  return <h2>react router</h2>;
+  // * exact(only exact matching) is very important because otherwise we will have the home page in all our components
+  return(
+    <Router>
+      <Navbar></Navbar>
+      <Switch>
+      <Route exact path='/'>
+        <Home></Home>
+      </Route>
+      <Route path='/about'>
+        <About></About>
+      </Route>
+      <Route path='/people'>
+        <People></People>
+      </Route>
+      <Route path='/person/:id' children={<Person></Person>}></Route>
+      <Route path='*'>
+        <Error></Error>
+      </Route>
+      </Switch>
+    </Router>
+  )
 };
 
 export default ReactRouterSetup;
